827311f3500b61155a33b5cf013c8f615376f8d7
aa.pahomov <aa.pahomov@tensor.ru>
2023-05-05 15:11:15 +0300

    commit

---
+++ dzdz/task1.py
@@ -0,0 +1,8 @@
+famyliya = 'Пахомов' #Gsbgsdgh
+imya = 'Андрей'
+stazh_raboty = '2 года и 3 месяца'
+dolzhnost = 'Инженер-тестировщик'  # Отдел: Тестирование базовой криптографии
+print('Фамилия:', famyliya)
+print('Имя:', imya)
+print('стаж работы:', stazh_raboty)
+print('должность:', dolzhnost)
