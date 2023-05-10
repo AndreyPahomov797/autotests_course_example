41687879b875362c95e3499b9d11d38480385bd9
aa.pahomov <aa.pahomov@tensor.ru>
2023-05-10 12:06:12 +0300

    commit

--- dzdz/dz3_1.py
+++ dzdz/dz3_1.py
@@ -2,9 +2,7 @@
 # В исходном списке минимум 2 элемента.
 
 def modification(lst):
-    lst0 = lst[-1]
-    lst[-1] = lst[0]
-    lst[0] = lst0
+    lst[0], lst[-1] = lst[-1], lst[0]
     # Здесь пишем код
     return lst
 
